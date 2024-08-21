92f315ed1d92dea7c2b66fb99cb2b6b33712b8b5
pinku2014 <166475063+pinku2014@users.noreply.github.com>
2024-08-21 13:01:54 +0530

    Jump Action

--- Assets/Scripts/MovementScripts/PlayerMovement.cs
+++ Assets/Scripts/MovementScripts/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.WebSockets;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,9 +8,20 @@ public class PlayerMovement : MonoBehaviour
 {
     Vector2 movementInput;
     public float movementSpeed = 0.1f;
+    public float gravity = 9.8f;
+    public float jumpSpeed = 20f;
+    public float verticalSpeed = 0;
     public void IAMovement(InputAction.CallbackContext context)
     {
         movementInput = context.ReadValue <Vector2>();
+    }
+
+    public void IAJump(InputAction.CallbackContext context)
+    {
+        if(context.started == true)
+        {
+            verticalSpeed = jumpSpeed;
+        }
     }
     // Start is called before the first frame update
     void Start()
@@ -20,6 +32,19 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(movementInput.x * movementSpeed * Time.deltaTime, 0, movementInput.y * movementSpeed *Time.deltaTime);
+        if (GroundCheck() == true)
+        {
+            verticalSpeed = 0; 
+        }
+        else
+        {
+            verticalSpeed = (verticalSpeed - gravity) * Time.deltaTime;
+        }
+        transform.Translate(movementInput.x * movementSpeed * Time.deltaTime, verticalSpeed, movementInput.y * movementSpeed *Time.deltaTime);
+    }
+
+    public bool GroundCheck()
+    {
+        return Physics.Raycast(transform.position, transform.up * -1, 1);
     }
 }
